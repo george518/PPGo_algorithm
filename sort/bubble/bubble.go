2f9d9e223183777a58bf13f47d8ae7621da51773
george <haodaquan2008@163.com>
2018-09-11 17:05:00 +0800

    冒泡排序新增几种优化方法

--- sort/bubble/bubble.go
+++ sort/bubble/bubble.go
@@ -7,19 +7,115 @@
 *************************************************************/
 package bubble
 
-//冒泡排序
+//冒泡排序（基础版)
+//最直观的冒泡排序
 func Sort(arr []int) []int {
 	len := len(arr)
 	if len < 2 {
 		return arr
 	}
 	for i := 1; i < len; i++ {
-		for j := 0; j < len-i; j++ {
+		//内层循环相邻进行比较
+		for j := 0; j < len-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j+1], arr[j] = arr[j], arr[j+1]
 			}
-			//fmt.Println(s)
+			//fmt.Println(arr)
 		}
+	}
+	return arr
+}
+
+//优化版2：观察可以发现，外层循环一次，内层循环完毕，此时最大值会被排在最后，
+// 外层循环n次，则最后n位上的数字也是排序好的，因此不需要排序
+func Sort2(arr []int) []int {
+	len := len(arr)
+	if len < 2 {
+		return arr
+	}
+	for i := 0; i < len-1; i++ {
+		//内层循环相邻进行比较
+		// len-i 之后的数据是排序好的，因为每次外层循环都是把大数字向后排
+		for j := 0; j < len-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+			}
+			//fmt.Println(arr)
+		}
+	}
+	return arr
+}
+
+// 优化版3 鸡尾酒排序法
+// 数组中的数字本是无规律的排放，先找到最小的数字，把他放到第一位，然后找到最大的数字放到最后一位。
+// 然后再找到第二小的数字放到第二位，再找到第二大的数字放到倒数第二位。以此类推，直到完成排序。
+func Sort3(arr []int) []int {
+	len := len(arr)
+	if len < 2 {
+		return arr
+	}
+
+	for i := 0; i < len/2; i++ {
+		for j := i; j < len-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+			}
+		}
+		for j := len - i - 1; j > i; j-- {
+			if arr[j] < arr[j-1] {
+				arr[j-1], arr[j] = arr[j], arr[j-1]
+			}
+		}
+	}
+	return arr
+}
+
+//优化4：如果内部循环没有发生交换行为，说明内部循环数组是排好序的，则跳出外部循环
+func Sort4(arr []int) []int {
+	len := len(arr)
+	if len < 2 {
+		return arr
+	}
+
+	for i := 0; i < len-1; i++ {
+		flag := true
+		for j := 0; j < len-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+				flag = false
+			}
+		}
+		if flag {
+			break
+		}
+	}
+	return arr
+}
+
+//优化5：如果内部循环没有发生交换行为，说明内部循环数组是排好序的，则跳出外部循环
+func Sort5(arr []int) []int {
+	len := len(arr)
+	if len < 2 {
+		return arr
+	}
+
+	k := len - 1
+	lastIndex := 0
+
+	for i := 0; i < len-1; i++ {
+		flag := true
+		for j := 0; j < k; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+				flag = false
+				lastIndex = j
+			}
+		}
+		if flag {
+			break
+		}
+
+		k = lastIndex
 	}
 	return arr
 }
--- sort/bubble/bubble_test.go
+++ sort/bubble/bubble_test.go
@@ -10,6 +10,8 @@ package bubble
 import (
 	"testing"
 
+	"fmt"
+
 	"github.com/george518/PPGo_algorithm/common"
 )
 
@@ -17,6 +19,55 @@ import (
 func TestSort(t *testing.T) {
 	arr := common.RandArray(10, 1000, 9999)
 	sortArr := Sort(arr)
+	fmt.Println(sortArr)
+	if !common.CheckArray(&sortArr) {
+		t.Error("failure")
+	} else {
+		t.Log("success")
+	}
+}
+
+//go test -v
+func TestSort2(t *testing.T) {
+	arr := common.RandArray(10, 1000, 9999)
+	sortArr := Sort2(arr)
+	fmt.Println(sortArr)
+	if !common.CheckArray(&sortArr) {
+		t.Error("failure")
+	} else {
+		t.Log("success")
+	}
+}
+
+//go test -v
+func TestSort3(t *testing.T) {
+	arr := common.RandArray(100, 1, 1000)
+
+	sortArr := Sort3(arr)
+	if !common.CheckArray(&sortArr) {
+		t.Error("failure")
+	} else {
+		t.Log("success")
+	}
+}
+
+//go test -v
+func TestSort4(t *testing.T) {
+	arr := common.RandArray(100, 1, 1000)
+
+	sortArr := Sort4(arr)
+	if !common.CheckArray(&sortArr) {
+		t.Error("failure")
+	} else {
+		t.Log("success")
+	}
+}
+
+//go test -v
+func TestSort5(t *testing.T) {
+	arr := common.RandArray(100, 1, 1000)
+
+	sortArr := Sort5(arr)
 	if !common.CheckArray(&sortArr) {
 		t.Error("failure")
 	} else {
@@ -33,5 +84,53 @@ func BenchmarkSort(b *testing.B) {
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		Sort(arr)
+	}
+}
+
+//go test -test.bench ".*"
+func BenchmarkSort2(b *testing.B) {
+	b.StopTimer() //调用该函数停止压力测试的时间计数
+	min, max := 1, b.N
+	len := 10000
+	arr := common.RandArray(len, min, max)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		Sort2(arr)
+	}
+}
+
+//go test -test.bench ".*"
+func BenchmarkSort3(b *testing.B) {
+	b.StopTimer() //调用该函数停止压力测试的时间计数
+	min, max := 1, b.N
+	len := 10000
+	arr := common.RandArray(len, min, max)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		Sort3(arr)
+	}
+}
+
+//go test -test.bench ".*"
+func BenchmarkSort4(b *testing.B) {
+	b.StopTimer() //调用该函数停止压力测试的时间计数
+	min, max := 1, b.N
+	len := 10000
+	arr := common.RandArray(len, min, max)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		Sort4(arr)
+	}
+}
+
+//go test -test.bench ".*"
+func BenchmarkSort5(b *testing.B) {
+	b.StopTimer() //调用该函数停止压力测试的时间计数
+	min, max := 1, b.N
+	len := 10000
+	arr := common.RandArray(len, min, max)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		Sort5(arr)
 	}
 }
